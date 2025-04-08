8846b407853e782087e9a547419a48f61e63feea
transcriptionapp <deal.conversion@gmail.com>
2025-04-08 14:05:22 +0200

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -164,7 +164,7 @@ googleBtn?.addEventListener("click", async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `https://transcriptionapp.github.io/VoiceNote/onboarding/welcome.html`,
+        redirectTo: `${window.location.origin}${window.location.pathname.replace("signup.html", "onboarding/welcome.html")}`,
       },
     });
 
