f8ad5901e27479b2e00ff5e679acd6ca6dde85c0
transcriptionapp <deal.conversion@gmail.com>
2025-04-08 15:27:57 +0200

    Update welcome.js

--- onboarding/welcome.js
+++ onboarding/welcome.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const startBtn = document.getElementById('start-btn');
+  const startBtn = document.getElementById('start-button');
   if (startBtn) {
     startBtn.addEventListener('click', () => {
       window.location.href = './role.html';
